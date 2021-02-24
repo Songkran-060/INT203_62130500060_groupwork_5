979659eea17d475d62274453f58230ddb23aa526
Songkran-060 <Pongpichet.sk@mail.kmutt.ac.th>
2021-02-24 10:44:53 +0700

    [EDIT] Arrange script

--- main.js
+++ main.js
@@ -1,74 +1,25 @@
 const app = {
   data() {
     return {
-      thaides: [{
-          name: 'Bu Lan Dan Mek',
-          url: './images/1.jpg',
-          isLike: false
-        },
-        {
-          name: 'Bu Lan Dan Mhok',
-          url: './images/2.jpg',
-          isLike: false
-        },
-        {
-          name: 'Ja Mong Kut (Original)',
-          url: './images/3.jpg',
-          isLike: false
-        },
-        {
-          name: 'Ja Mong Kut (Now)',
-          url: './images/4.jpg',
-          isLike: false
-        },
-        {
-          name: 'Leum Guen',
-          url: './images/5.jpg',
-          isLike: false
-        },
-        {
-          name: 'Phra Pai',
-          url: './images/6.jpg',
-          isLike: false
-        },
-        {
-          name: 'Cho maung',
-          url: './images/7.jpg',
-          isLike: false
-        },
-        {
-          name: 'Look Chub',
-          url: './images/8.jpg',
-          isLike: false
-        },
-        {
-          name: 'Thong Eak',
-          url: './images/9.jpg',
-          isLike: false
-        }
-      ],
+      thaides: [{name: 'Bu Lan Dan Mek',url: './images/1.jpg',isLike: false},
+                {name: 'Bu Lan Dan Mhok',url: './images/2.jpg',isLike: false},
+                {name: 'Ja Mong Kut (Original)',url: './images/3.jpg',isLike: false},
+                {name: 'Ja Mong Kut (Now)',url: './images/4.jpg',},
+                {name: 'Leum Guen',url: './images/5.jpg',isLike: false},
+                {name: 'Phra Pai',url: './images/6.jpg',isLike: false},
+                {name: 'Cho maung',url: './images/7.jpg',isLike: false},
+                {name: 'Look Chub',url: './images/8.jpg',isLike: false},
+                {name: 'Thong Eak',url: './images/9.jpg',isLike: false} ],
       inputs: '',
-      searchBoxOpen: false,
       searchFound: false,
       showImages: false,
-      currentIndex: 0
+      currentIndex: 0 
     }
   },
   methods: {
     likeIt(index) {
       this.thaides[index].isLike = !this.thaides[index].isLike
     },
-    switchSearch() {
-      this.searchBoxOpen = !this.searchBoxOpen
-      this.inputs = ''
-    },
-    showImg(index) {
-      this.showImages = true;
-      this.currentIndex = index;
-    },
-    hideImg() {
-      this.showImages = false;
-    }
     
   },
   computed: {
@@ -92,4 +43,3 @@ const app = {
   }
 
 }
-Vue.createApp(app).mount('#app')
